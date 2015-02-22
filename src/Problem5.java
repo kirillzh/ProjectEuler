b30e2c7a170a53eb24b4646fa791105bc79b054b
Kirill Zhukov <kirill@cloudcar.com>
2015-02-22 07:30:04 -0800

    Solution for for problem #5

--- src/Problem5.java
+++ src/Problem5.java
@@ -3,5 +3,30 @@
  * <p/>
  * What is the smallest positive number that is evenly divisible (divisible with no remainder) by all of the numbers from 1 to 20?
  */
+
 public class Problem5 {
+
+    public static void main(String[] args) {
+
+        long wantedNumber = 1;
+        for (int i = 2; i <= 20; i++)
+            wantedNumber *= i;
+
+        for (int i = 2; i <= 20; i++) {
+
+            while (isDivisible(wantedNumber / i))
+                wantedNumber /= i;
+        }
+
+        System.out.println(wantedNumber);
+
+    }
+
+    private static boolean isDivisible(long number) {
+        for (int i = 2; i <= 20; i++)
+            if (number % i != 0)
+                return false;
+        return true;
+    }
+
 }
