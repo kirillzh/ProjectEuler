42e798a2b13a63ebaf5b093bcc8d4c0e88d4f503
Kirill Zhukov <kirill@cloudcar.com>
2015-02-22 13:50:13 -0800

    Solved the problem #7

--- src/Problem7.java
+++ src/Problem7.java
@@ -1,5 +1,34 @@
+import java.util.LinkedList;
+
 /**
- * Created by kirill on 2/22/15.
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * <p/>
+ * What is the 10 001st prime number?
  */
+
 public class Problem7 {
+
+    public static void main(String[] args) {
+        LinkedList<Long> primes = new LinkedList<Long>();
+        primes.add(1L);
+        while (primes.size() <= 10001) {
+            primes.add(nextPrime(primes.getLast()));
+        }
+
+        System.out.println(primes.get(10001));
+
+    }
+
+    public static boolean isPrime(long n) {
+        for (long i = 2; i < n; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static long nextPrime(long n) {
+        for (long i = n + 1; ; i++) {
+            if (isPrime(i)) return i;
+        }
+    }
+
 }
